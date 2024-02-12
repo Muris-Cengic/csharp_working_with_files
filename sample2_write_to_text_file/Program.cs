dba7479d89e7bce166dac87a133bdccbc0fa7ab3
Muris.Cengic <muris.cengic@outlook.com>
2024-02-12 09:50:14 +0400

    Add sample2 to write to a text file

---
+++ sample2_write_to_text_file/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace sample2_write_to_text_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // After the program is compiled it goes to <project_folder>\bin\Debug\netx.0\ folder
+            // if we want to create a file in the same folder as this program before compailation we need to go back to project_folder
+            StreamWriter writer = new StreamWriter(@"..\..\..\HelloWorldCreated.txt");
+            writer.WriteLine("Hello World!!");
+            writer.WriteLine("From the StreamWriter class");
+            writer.Close();
+        }
+    }
+}
